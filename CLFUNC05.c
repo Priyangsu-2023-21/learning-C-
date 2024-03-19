a59d731fde3c00af14ebe83970f2a5ebdf741ae9
Priyangsu-2023-21 <162979594+Priyangsu-2023-21@users.noreply.github.com>
2024-03-19 07:13:08 +0530

    CLFUNC05.c

---
+++ CLFUNC05.c
@@ -0,0 +1,29 @@
+
+
+
+\\Update the function given in the IDE to output the following to the IDE by printing from inside the function.
+
+
+
+
+
+
+
+
+
+#include <stdio.h>
+void compute_value(int a, int b) {
+    // update your code below this line
+    
+}
+
+int main() {
+    int t = 3;
+
+    for (int i = 0; i < t; i++) {
+        int A, B;
+        scanf("%d %d", &A, &B);
+        compute_value(A, B);
+    }
+  
+}
